2ffd2ade9d04cee0b08772a285c2b92049a32443
SnrPepe <zubarevkostia@gmail.com>
2025-02-13 14:07:02 +1000

    cat pre final

--- cat/main.c
+++ cat/main.c
@@ -1,23 +1,72 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-void print_file(char *name) {
-    FILE *f = fopen(name, "rt");
+int number_nonblank = 0;
+int show_ends = 0;
+int number_all = 0;
+int squeeze_blank = 0;
+int show_tabs = 0;
 
-    if (f != NULL) {
-        int c = fgetc(f);
-        while (c != EOF) {
-            putc(c, stdout);
-            c = fgetc(f);
+void cat_file(FILE *file) {
+    int c, prev = '\n';
+    int line_number = 1;
+    while ((c = fgetc(file)) != EOF) {
+        if (squeeze_blank && c == '\n' && prev == '\n') {
+            continue;
         }
-        fclose(f);
+        if (number_nonblank && c != '\n' && (prev == '\n' || line_number == 1)) {
+            printf("%6d\t", line_number++);
+        }
+        if (number_all && (prev == '\n' || line_number == 1)) {
+            printf("%6d\t", line_number++);
+        }
+        if (show_tabs && c == '\t') {
+            printf("^I");
+            prev = c;
+            continue;
+        }
+        if (show_ends && c == '\n') {
+            printf("$");
+        }
+        putchar(c);
+        prev = c;
     }
 }
 
-int main(int argc, char **argv) {
-  for (int i = 1; i < argc; i++) {
-    print_file(argv[i]);
-  }
-  return 0;
+int main(int argc, char *argv[]) {
+    int i, j;
+    for (i = 1; i < argc; i++) {
+        if (argv[i][0] == '-') {
+            for (j = 1; argv[i][j] != '\0'; j++) {
+                switch (argv[i][j]) {
+                    case 'b': number_nonblank = 1; break;
+                    case 'e': show_ends = 1; break;
+                    case 'n': number_all = 1; break;
+                    case 's': squeeze_blank = 1; break;
+                    case 't': show_tabs = 1; break;
+                    default:
+                        fprintf(stderr, "Unknown option: -%c\n", argv[i][j]);
+                        return EXIT_FAILURE;
+                }
+            }
+        } else {
+            break;
+        }
+    }
+
+    if (i == argc) {
+        cat_file(stdin);
+    } else {
+        for (; i < argc; i++) {
+            FILE *file = fopen(argv[i], "r");
+            if (file == NULL) {
+                perror("cat");
+                return EXIT_FAILURE;
+            }
+            cat_file(file);
+            fclose(file);
+        }
+    }
+    return EXIT_SUCCESS;
 }
